cab1506555d7bc5627e40628165f0b1dd846026a
S.Lazarenko <ajaxamsterdam878@gmail.com>
2020-03-07 13:06:52 +0200

    Updates

---
+++ precache-manifest.dfaa1a0d0f72460b5ef61f7e5b9f1498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdbbdc216ea1fd2d86e7b7908a89c0a",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "fc49c0588c762695f58c",
+    "url": "/goit-react-hw-01-components/static/css/main.156d0b42.chunk.css"
+  },
+  {
+    "revision": "ef4d354372a6708656c3",
+    "url": "/goit-react-hw-01-components/static/js/2.5600e26a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.5600e26a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc49c0588c762695f58c",
+    "url": "/goit-react-hw-01-components/static/js/main.30dbabdd.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
